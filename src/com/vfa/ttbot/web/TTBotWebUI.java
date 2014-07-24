85d3ee5a0a7fad16a555830cef994dc15cb112f3
betovf <alberto@albertovelasco.com>
2014-07-24 18:36:41 +0200

    Fix y-axis labels. Rename chart. Expand width of chart. Add form with date fields and submit button to refresh the data shown in chart.

--- src/com/vfa/ttbot/web/TTBotWebUI.java
+++ src/com/vfa/ttbot/web/TTBotWebUI.java
@@ -14,10 +14,19 @@ import com.vaadin.addon.charts.model.ChartType;
 import com.vaadin.addon.charts.model.Configuration;
 import com.vaadin.addon.charts.model.DataSeries;
 import com.vaadin.addon.charts.model.DataSeriesItem;
+import com.vaadin.addon.charts.model.Series;
 import com.vaadin.addon.charts.model.XAxis;
 import com.vaadin.addon.charts.model.YAxis;
 import com.vaadin.annotations.Theme;
+import com.vaadin.data.Validator;
+import com.vaadin.data.validator.DateRangeValidator;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.shared.ui.datefield.Resolution;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.PopupDateField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vfa.ttbot.helper.ModelHelper;
@@ -35,89 +44,172 @@ public class TTBotWebUI extends UI {
 	private List<Trend> trends;
 	private Map<Date, List<TrendLog>> mapTrendLogs;
 	private ArrayList<Date> dates;
+	private Date minDate;
+	private Date endDate;
+	private Date iniDate;
+	private Chart chart;
+	private PopupDateField iniDateField;
+	private PopupDateField endDateField;
 	
 	@Override
 	protected void init(VaadinRequest request) {
+		// Main layout
 		final VerticalLayout layout = new VerticalLayout();
 		layout.setMargin(true);
+		layout.setSpacing(true);
 		setContent(layout);
 
-		Chart chart = new Chart(ChartType.LINE);
-		chart.setWidth("1000px");
+		// Chart
+		chart = new Chart(ChartType.LINE);
+		chart.setWidth("100%");
 		chart.setHeight("500px");
 		        
-		// Modify the default configuration a bit
+		// Modify configuration
 		Configuration conf = chart.getConfiguration();
-		conf.setTitle("TTs");
-		conf.setSubTitle("Evolución de TTs en España");
-		//conf.getLegend().setEnabled(false); // Disable legend
-
-		// The data
-		this.loadData();
+		conf.setTitle("TTBot.es");
+		conf.setSubTitle("Evolución de Trending Topics en España");
 		
-		for (Trend trend : this.trends) {
-			DataSeries series = new DataSeries(trend.getName());
-			
-			for (Date date : this.dates) {
-				List<TrendLog> logs = this.mapTrendLogs.get(date);
-				Integer pos = null;
-				for (TrendLog log : logs) {
-					if (log.getIdTrend().equals(trend.getId())) {
-						pos = log.getPosition();
-					}
-				}
-				DataSeriesItem item = new DataSeriesItem(date,pos);
-				series.add(item);
-			}
-			conf.addSeries(series);
-		}
-		
-		// Set the category labels on the axis correspondingly
+		// Set the X axis to handle date-time
 		XAxis xaxis = new XAxis();
 		xaxis.setType(AxisType.DATETIME);
 		xaxis.setTitle("Fecha-Hora");
 		conf.addxAxis(xaxis);
 
-		// Set the Y axis title
+		// Set the Y axis to show positions from 1 to 10 
 		YAxis yaxis = new YAxis();
 		yaxis.setTitle("Posición");
+		yaxis.setTickInterval(1);
 		yaxis.setMin(1);
 		yaxis.setMax(10);
 		yaxis.setReversed(true);
-//		String[] categories = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
-//		yaxis.setCategories(categories);
-		//yaxis.getLabels().setFormatter("Math.abs(this.y-10)");
 		conf.addyAxis(yaxis);
-		        
+		
+		// First time drawing
+		this.initDates();
+		this.loadData();		
+		this.populateChart();
+		
 		layout.addComponent(chart);
+		
+		// Form fields
+		HorizontalLayout formLayout = new HorizontalLayout();
+		formLayout.setSpacing(true);
+		layout.addComponent(formLayout);
+
+		iniDateField = new PopupDateField("Fecha-hora de inicio", this.iniDate);
+		iniDateField.setResolution(Resolution.MINUTE);
+		iniDateField.setRequired(true);
+		iniDateField.setRequiredError("Debe especificar la fecha de inicio");
+		iniDateField.addValidator(new DateRangeValidator("Fecha de inicio fuera de rango: no puede ser anterior a 24 horas ni posterior a la hora actual", this.minDate, this.endDate, Resolution.MINUTE));
+		iniDateField.setImmediate(true);
+		endDateField = new PopupDateField("Fecha-hora de fin", this.endDate);
+		endDateField.setResolution(Resolution.MINUTE);
+		endDateField.setRequired(true);
+		endDateField.setRequiredError("Debe especificar la fecha de fin");
+		endDateField.addValidator(new Validator() {
+			@Override
+			public void validate(Object value) throws InvalidValueException {
+				// Check against initial date
+				if (!(value instanceof Date && ((Date)value).after(iniDateField.getValue()))) {
+					throw new InvalidValueException("La fecha de fin tiene que ser posterior a la de inicio");
+				}
+			}			
+		});
+		endDateField.setImmediate(true);
+		
+		Button button = new Button("Enviar");
+		button.addClickListener(new Button.ClickListener() {
+		    public void buttonClick(ClickEvent event) {
+		    	// Proceed only if both dates are valid (validators run immediately)
+				if(iniDateField.isValid() && endDateField.isValid()) {
+					refreshChart();
+				}
+		    }
+		});
+		
+		formLayout.addComponent(iniDateField);
+		formLayout.addComponent(endDateField);
+		formLayout.addComponent(button);
+		formLayout.setComponentAlignment(button, Alignment.BOTTOM_CENTER);
+		
 	}
 
+	protected void refreshChart() {
+		// Get new dates 
+		this.iniDate = this.iniDateField.getValue();
+		this.endDate = this.endDateField.getValue();
+		
+		// Reload data in chart
+		this.loadData();		
+		this.populateChart();
+		
+		this.chart.drawChart();
+	}
+
+	private void initDates() {
+		
+		this.endDate = new Date();
+		GregorianCalendar calendar = new GregorianCalendar();
+		calendar.setTime(endDate);
+		calendar.add(Calendar.HOUR_OF_DAY, -2);
+		this.iniDate = calendar.getTime();
+		
+		// Can't show beyond one day in the past
+		calendar.add(Calendar.HOUR_OF_DAY, -22);
+		this.minDate = calendar.getTime();
+	}
+	
 	private void loadData() {
-		//SimpleDateFormat sdFmt = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-		
 		try {
-			Date end = new Date();//sdFmt.parse("2014-02-22 22:28");//
-			GregorianCalendar calendar = new GregorianCalendar();
-			calendar.setTime(end);
-			calendar.add(Calendar.HOUR_OF_DAY, -2);
-			Date ini = calendar.getTime();
-			
-			this.trendLogs = dataService.getTrendLogsByDate(ini, end);
+			// Get all logs from date to date
+			this.trendLogs = dataService.getTrendLogsByDate(this.iniDate, this.endDate);
 		
-			this.mapTrendLogs = ModelHelper.groupTrendLogsByDateTime(trendLogs);
+			// Group those logs by date-time
+			this.mapTrendLogs = ModelHelper.groupTrendLogsByDateTime(this.trendLogs);
 			
-			this.dates = new ArrayList<Date>(mapTrendLogs.keySet());
+			// Get list of dates from previous map
+			this.dates = new ArrayList<Date>(this.mapTrendLogs.keySet());
 			Collections.sort(this.dates);
 			
-			Map<Integer, List<TrendLog>> mapTrends = ModelHelper.groupTrendLogsByTrend(trendLogs);
+			// Group log also by trend
+			Map<Integer, List<TrendLog>> mapTrends = ModelHelper.groupTrendLogsByTrend(this.trendLogs);
 			
+			// Get trends names from id-trend list from previous map
 			this.trends = ModelHelper.getTrends(dataService, new ArrayList<Integer>(mapTrends.keySet()));
 			
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		
-		
 	}
-	
+
+	private void populateChart() {
+		// New series
+		List<Series> newSeries = new ArrayList<Series>();
+		
+		// Configure series according to data
+		for (Trend trend : this.trends) {
+			// Each trend will be a series
+			DataSeries series = new DataSeries(trend.getName());
+			
+			// Loop over all possible date-times
+			for (Date date : this.dates) {
+				// Loop over all log positions
+				List<TrendLog> logs = this.mapTrendLogs.get(date);
+				Integer pos = null;
+				for (TrendLog log : logs) {
+					// Search a match to current trend
+					if (log.getIdTrend().equals(trend.getId())) {
+						pos = log.getPosition();
+					}
+				}
+				// pos might be null if current trend did no appear in the top ten that time
+				DataSeriesItem item = new DataSeriesItem(date,pos);
+				series.add(item);
+			}
+			// Add trend series
+			newSeries.add(series);
+		}
+		// Set series to chart (discarding previous ones)
+		this.chart.getConfiguration().setSeries(newSeries);
+	}
 }
